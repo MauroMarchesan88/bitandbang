14fb04ce752427262153e747ee4b944b19a8896f
Mauro <MauroMarchesan88@gmail.com>
2022-08-08 19:36:53 -0300

    Altering som data

--- build.js
+++ build.js
@@ -15,18 +15,18 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('           Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  shorthandle: chalk.white('bnb'),
-  work: chalk.white('Staff Developer Advocate at Twilio'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('           Mauro Marchesan'),
+  // handle: chalk.white('Mauro_Marchesan88'),
+  // shorthandle: chalk.white('bnb'),
+  work: chalk.white('Fullstack Web Developer'),
+  // twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~mauromarchesan'),
+  github: chalk.gray('https://github.com/') + chalk.green('MauroMarchesan88'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('mauro-marchesan'),
+  web: chalk.cyan('https://portfolio-mauro.netlify.app/'),
+  npx: chalk.red('npx') + ' ' + chalk.white('mauromarchesan'),
   labelWork: chalk.white.bold('    Work:'),
-  labelTwitter: chalk.white.bold(' Twitter:'),
+  // labelTwitter: chalk.white.bold(' Twitter:'),
   labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
   labelLinkedIn: chalk.white.bold('LinkedIn:'),
@@ -36,9 +36,10 @@ const data = {
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} / ${data.handle} / ${data.shorthandle}`
+const heading = `${data.name}`
+//  / ${data.handle} / ${data.shorthandle}
 const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
+// const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
@@ -47,13 +48,13 @@ const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+  newline + newline + // Add one whole blank line
+  working + newline + // data.labelWork + data.work
+  //  twittering + newline + // data.labelTwitter + data.twitter
+  npming + newline + // data.labelnpm + data.npm
+  githubing + newline + // data.labelGitHub + data.github
+  linkedining + newline + // data.labelLinkedIn + data.linkedin
+  webing + newline + newline + // data.labelWeb + data.web
+  carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
